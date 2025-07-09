08a8e95cdba6a409c76e20b2db577438520c781e
joel <joelliceras71@gmail.com>
2025-07-09 15:50:23 -0400

    estructura inicial de proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'welcome',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'welcome',
+    loadComponent: () => import('./features/welcome/pages/welcome/welcome.page').then( m => m.WelcomePage)
   },
 ];
